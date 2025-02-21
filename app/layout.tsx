2b816dd5bb3ca61eb8e1fe29113c8536f6ba5e99
Robert Mill <81169127+RobertVMill@users.noreply.github.com>
2025-02-21 10:55:03 -0500

    fix: update theme provider types

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 import { Providers } from "./providers";
+import "./globals.css";
 import Navbar from '@/components/Navbar'
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Robert Mill - Portfolio",
